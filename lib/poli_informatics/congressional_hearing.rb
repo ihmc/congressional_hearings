797ff078859cd5cea1310bfc9ab5d9257dfe471f
Adam Dalton <adalton@ihmc.us>
2014-03-20 00:13:38 -0400

    escaping double quotes

--- lib/poli_informatics/congressional_hearing.rb
+++ lib/poli_informatics/congressional_hearing.rb
@@ -6,6 +6,7 @@ module PoliInformatics
       source.gsub!(/\[GRAPHIC\]\s*\[TIFF OMITTED\].*/,'')
       source.gsub!(/\[.*?\]/m,'')
       source.gsub!(/<.*?>/m,'')
+      source.gsub!('"','""')
       source.lines.each do |line|
         break if line.strip == 'A P P E N D I X'
         transcript.ingest line
